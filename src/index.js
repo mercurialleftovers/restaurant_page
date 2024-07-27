8ba6a3184994dfb748d7237d70512be4d2786de9
mercurialleftovers <mercurialleftovers@proton.me>
2024-07-27 16:33:27 +0100

    css loading works, html-webpack-plugin configured to output according to a template in src

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import css_style from './style.css'
+
+const PI = 3.14
+
+console.log(PI)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlPlugin = require('html-webpack-plugin')
+const path       = require('path')
+
+
+module.exports = {
+    mode: 'development',
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    plugins: [
+        new HtmlPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
